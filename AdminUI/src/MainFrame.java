8505ab05f9841f7bfa5ba0563340c22611758a46
Denscape <dnmrrdnd@gmail.com>
2025-05-29 15:46:17 +0800

    initial commit

---
+++ AdminUI/src/MainFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MainFrame {
+
+
+    private JPanel mainFrame;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+
+}
